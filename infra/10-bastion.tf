4dcd324d8100d9e72df7c1b2ce9a51d448b204c0
pmh-only <opensource@pmh.codes>
2023-07-01 13:53:54 +0900

    add infra

---
+++ infra/10-bastion.tf
@@ -0,0 +1,87 @@
+resource "aws_eip" "bastion" {
+  vpc = true
+  instance = aws_instance.bastion.id
+  associate_with_private_ip = aws_instance.bastion.private_ip
+}
+
+resource "aws_security_group" "bastion" {
+  name = "argocd-bastion-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = "22"
+    to_port = "22"
+  }
+
+  egress {
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = "0"
+    to_port = "0"
+  }
+}
+
+resource "aws_iam_role" "bastion" {
+  name = "argocd-role-bastion"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "argocd-profile-bastion"
+  role = aws_iam_role.bastion.name
+}
+
+resource "aws_instance" "bastion" {
+  instance_type = "c5.large"
+  subnet_id = aws_subnet.public_a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  key_name = aws_key_pair.keypair.key_name
+  
+  ami = "ami-035da6a0773842f64"
+
+  tags = {
+    Name = "argocd-bastion"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    ./aws/install
+
+    ARCH=amd64
+    PLATFORM=$(uname -s)_$ARCH
+
+    curl -sLO "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+    tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+    mv /tmp/eksctl /usr/local/bin
+
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+
+    yum install -y jq curl
+  EOF
+}
